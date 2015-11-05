4ff5dd13d82eb442f46bbb77726524bab50627b3
Christopher Hunt <huntchr@gmail.com>
2015-11-05 12:32:18 +1100

    Corrected the init order for tox
    Relying on a constructor to actually perform side-effecting things it awful, yet tox does it. This commit repairs that problem.

--- setup.py
+++ setup.py
@@ -24,10 +24,10 @@ class Tox(test):
     user_options = [('tox-args=', 'a', 'Arguments to pass to tox')]
 
     def __init__(self, dist, **kw):
-        super().__init__(dist, **kw)
         self.test_suite = True
         self.test_args = []
         self.tox_args = None
+        super().__init__(dist, **kw)
 
     def initialize_options(self):
         test.initialize_options(self)
